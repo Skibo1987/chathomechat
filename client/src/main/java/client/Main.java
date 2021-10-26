b071a148059edd82b006b8c2716ccf7b37627f35
geekbrains <info@geekbrains.ru>
2021-10-26 02:28:46 -0700

    Chat_Home_SQL

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -7,12 +7,31 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main extends Application {
     private static Connection connection;
     private static Statement stmt;
 
+    public static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc: sqlite: main.db ");
+        stmt = connection.createStatement();
+    }
+    public static void disconnect(){
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try {
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
